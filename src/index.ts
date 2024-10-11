df8c36dd5391309b7980db09dff694ed2a49109a
Nathan Seymour <nathan@seymour.global>
2024-10-11 09:17:36 -0500

    add simple counter to site;

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,10 @@ import express from 'express';
 
 const app = express();
 
+let count = 0;
+
 app.get('/', (req, res) => {
-  res.send('Welcome to the API!');
+  res.send(`Times visited since last deployment: ${++count}`);
 });
 
 app.listen(process.env.PORT || 3000);
